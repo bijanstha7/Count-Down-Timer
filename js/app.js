efd27dd61a43617a2418e3f6e6365e79daed93c8
Bijan Shrestha <bijan.shrestha7170@gmail.com>
2023-08-31 00:12:38 +0545

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const endDate = "1 January 2024 12:00:00 AM";
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+
+  if (diff < 0) return;
+
+  // convert into days;
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+}
+
+// initial call
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
